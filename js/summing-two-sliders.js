5e5688fb9e34c37ba099ab7b404c5bd396759c5b
Pete Hodgson <git@thepete.net>
2014-10-05 11:56:46 -0700

    summing-two-sliders: heres one I made earlier

--- js/summing-two-sliders.js
+++ js/summing-two-sliders.js
@@ -2,32 +2,9 @@ var inputEventToVal = function(e){
   return $(e.target).val();
 };
 
-var summer = function(a,b){
-  return a + b;
-};
-
 $( function(){
-  var sliderAValues = 
-    $('.slider.a input')
-    .asEventStream('input',inputEventToVal)
-    .map( parseInt );
-
-  var sliderBValues = 
-    $('.slider.b input')
-    .asEventStream('input',inputEventToVal)
-    .map( parseInt );
-
-  sliderAValues
-    .visualize('slider a')
-    .assign( $('.slider.a .label'), 'text' );
-
-  sliderBValues
-    .visualize('slider b')
-    .assign( $('.slider.b .label'), 'text' );
-
-  //var combinedStream = sliderAValues.merge(sliderBValues);
-  var combinedStream = Bacon.combineWith( summer, sliderAValues, sliderBValues )
-  combinedStream
-    .visualize( 'both sliders' )
-    .assign( $('.sum .label'), 'text' );
+  var $sliderA = $(".slider.a input"),
+      $labelA = $(".slider.a .label"),
+      $sliderB = $(".slider.b input"),
+      $labelB = $(".slider.b .label");
 });
