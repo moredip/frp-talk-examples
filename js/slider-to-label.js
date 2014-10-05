3f65fd8cd4ea1b49acb3d3854021c21d7c6616f1
Pete Hodgson <git@thepete.net>
2014-10-05 15:47:20 -0700

    TALK STARTING POINT

--- js/slider-to-label.js
+++ js/slider-to-label.js
@@ -5,19 +5,4 @@ var inputEventToVal = function(e){
 $( function(){
   var $slider = $('.slider input'),
       $label = $('.slider .label');
-
-  var stream = $slider
-    .asEventStream( 'input', inputEventToVal )
-    .visualize("raw value")
-    .map( parseFloat )
-    .visualize("parsed")
-    .map( function(x){
-      return Math.round(x*100);
-    })
-    .visualize("as percentage")
-    .map( function(x){
-      return ""+x+"%";
-    });
-
-  stream.assign( $label, "text" );
 });
--- js/summing-two-sliders.js
+++ js/summing-two-sliders.js
@@ -2,33 +2,10 @@ var inputEventToVal = function(e){
   return $(e.target).val();
 };
 
-var summer = function(a,b){
-  return a + b;
-};
-
-var streamFromSlider = function($slider){
-  return $slider
-    .asEventStream( "input", inputEventToVal )
-    .toProperty( $slider.val() )
-    .map( parseFloat );
-};
-
 $( function(){
   var $sliderA = $(".slider.a input"),
       $labelA = $(".slider.a .label"),
       $sliderB = $(".slider.b input"),
-      $labelB = $(".slider.b .label");
-
-  var streamA = streamFromSlider($sliderA);
-  streamA.visualize("stream a");
-  streamA.assign( $labelA, "text" );
-
-  var streamB = streamFromSlider($sliderB);
-  streamB.visualize("stream b");
-  streamB.assign( $labelB, "text" );
-
-  var combinedStream = Bacon.combineWith( summer, streamA, streamB );
-  combinedStream.visualize("combined");
-
-  combinedStream.assign( $(".sum .label"), "text" );
+      $labelB = $(".slider.b .label"),
+      $summedLabel = $('.sum .label');
 });
